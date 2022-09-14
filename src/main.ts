063c2bb4a7c8147245dedf71dd692c79a55dff3f
xunziheng <xunziheng88@163.com>
2022-09-14 21:52:18 +0800

    wip

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ import wx from 'weixin-js-sdk';
 
 // wx.config({
 //   debug: true, // 开启调试模式,调用的所有 api 的返回值会在客户端 alert 出来，若要查看传入的参数，可以在 pc 端打开，参数信息会通过 log 打出，仅在 pc 端时才会打印。
-//   appId: 'wx6550244a92015fa4', // 必填，公众号的唯一标识
+//   appId: '', // 必填，公众号的唯一标识
 //   timestamp: , // 必填，生成签名的时间戳
 //   nonceStr: '', // 必填，生成签名的随机串
 //   signature: '',// 必填，签名
