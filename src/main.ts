7db9938ffb18de495a740bd7172b9eb0f116926f
xunziheng <xunziheng88@163.com>
2022-09-19 10:29:43 +0800

    wip

--- src/main.ts
+++ src/main.ts
@@ -23,37 +23,72 @@ get('/jssdkSignatureGet', {
     jsApiList: [
       'updateAppMessageShareData',
       'updateTimelineShareData',
+      'onMenuShareTimeline',
+      'onMenuShareAppMessage',
+      'onMenuShareQQ',
+      'onMenuShareWeibo',
+      'onMenuShareQZone',
+      'startRecord',
+      'stopRecord',
+      'onVoiceRecordEnd',
+      'playVoice',
+      'pauseVoice',
+      'stopVoice',
+      'onVoicePlayEnd',
+      'uploadVoice',
+      'downloadVoice',
+      'chooseImage',
+      'previewImage',
+      'uploadImage',
+      'downloadImage',
+      'translateVoice',
+      'getNetworkType',
+      'openLocation',
+      'getLocation',
+      'hideOptionMenu',
+      'showOptionMenu',
       'hideMenuItems',
+      'showMenuItems',
+      'hideAllNonBaseMenuItem',
+      'showAllNonBaseMenuItem',
+      'closeWindow',
+      'scanQRCode',
+      'chooseWXPay',
+      'openProductSpecificView',
+      'addCard',
+      'chooseCard',
+      'openCard',
     ] // 必填，需要使用的 JS 接口列表
   });
 
   wx.ready(function () {
     //需在用户可能点击分享按钮前就先调用
-    wx.updateAppMessageShareData({ 
-      title: 'test 分享 哈喽', // 分享标题
+    wx.onMenuShareAppMessage({
+      title: 'test 分享', // 分享标题
       desc: '描述', // 分享描述
-      link: window.location.href, // 分享链接，该链接域名或路径必须与当前页面对应的公众号 JS 安全域名一致
+      link: 'http://local.xunziheng.top', // 分享链接，该链接域名或路径必须与当前页面对应的公众号 JS 安全域名一致
       imgUrl: 'http://local.xunziheng.top/share_icon.png', // 分享图标
+      type: 'link', // 分享类型,music、video或link，不填默认为link
+      dataUrl: '', // 如果 type 是music或video，则要提供数据链接，默认为空
       success: function () {
-        // 设置成功
-        console.log('设置成功')
-      }
-    });
-    wx.updateTimelineShareData({ 
-      title: 'test朋友圈', // 分享标题
-      link: window.location.href, // 分享链接，该链接域名或路径必须与当前页面对应的公众号 JS 安全域名一致
-      imgUrl: 'http://local.xunziheng.top/share_icon.png', // 分享图标
-      success: function () {
-        // 设置成功
-        console.log('设置成功')
+        console.log('设置成功');
+        // 用户点击了分享后执行的回调函数
       }
     })
-  });
+    // 即将废弃的api，不过个人未认证的订阅号只能获取到这个权限
+    wx.onMenuShareTimeline({
+      title: 'test朋友圈', // 分享标题
+      link: 'http://local.xunziheng.top', // 分享链接，该链接域名或路径必须与当前页面对应的公众号 JS 安全域名一致
+      imgUrl: 'http://local.xunziheng.top/share_icon.png', // 分享图标
+      success: function () {
+        // 用户点击了分享后执行的回调函数
+        console.log('设置成功');
+      }
+    });
 
-  wx.error(function(res){
-    // config信息验证失败会执行 error 函数，如签名过期导致验证失败，具体错误信息可以打开 config 的debug模式查看，也可以在返回的 res 参数中查看，对于 SPA 可以在这里更新签名。
-    console.log('微信API失败：', res)
-  });  
+    // 隐藏基本类按钮
+    wx.hideAllNonBaseMenuItem();
+  });
 })
 
 const app = new App({
